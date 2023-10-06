a110fae0493daca978ff05e5ff49428db614f74a
Daniel Peñaloza <danielpenaloza@Daniels-MacBook-Pro.local>
2023-10-06 17:10:06 -0500

    Show All Characters: Add new material theme

--- app/src/main/java/com/example/rickandmorty/characters/CharactersScreen.kt
+++ app/src/main/java/com/example/rickandmorty/characters/CharactersScreen.kt
@@ -1,10 +1,34 @@
 package com.example.rickandmorty.characters
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rickandmorty.ui.theme.RickAndMortyTheme
+
 
 @Composable
 fun CharactersScreen(
     modifier: Modifier = Modifier
 ) {
+    Surface(
+        modifier = modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ){
+
+    }
+}
+
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES ,
+    name = "Dark"
+)
+@Composable
+fun PreviewCharactersCreen(){
+    RickAndMortyTheme {
+        CharactersScreen()
+    }
 }
